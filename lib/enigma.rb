bb31379580e107a6cafc37d78a92102175d84b85
Shirley DeCesari <shirley.decesari@gmail.com>
2022-06-11 14:53:52 -0700

    Feat -Add 'generate_key' method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,7 +1,11 @@
 class Enigma
-attr_reader :character_set
-def initialize
-  @character_set = ("a".."z").to_a << " "
-end
+  attr_reader :character_set
+  def initialize
+    @character_set = ("a".."z").to_a << " "
+  end
+
+  def generate_key
+    rand(99999).to_s.rjust(5, '0')
+  end
 
 end
