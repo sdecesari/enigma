d4f3a10e81ff24159add564882755052ffc6c19d
Shirley DeCesari <shirley.decesari@gmail.com>
2022-06-11 20:37:57 -0700

    Test -Add 'character_set'

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe Generator do
     expect(generator).to be_a Generator
   end
 
+  it "can have an array of characters" do
+    expect(generator.character_set).to be_a Array
+    expect(generator.character_set.count).to eq (27)
+  end
+
 end
