4cff65a31de3637f2b71f43a52a56bea0b7990af
Shirley DeCesari <shirley.decesari@gmail.com>
2022-06-11 20:45:34 -0700

    Feat -Add 'split_keys' method

--- lib/generator.rb
+++ lib/generator.rb
@@ -4,4 +4,18 @@ class Generator
     @character_set = ("a".."z").to_a << " "
   end
 
+  def generate_key
+    rand(99999).to_s.rjust(5, '0')
+  end
+
+  def split_keys
+    key = generate_key
+    {
+      a_key: key[0..1].to_i,
+      b_key: key[1..2].to_i,
+      c_key: key[2..3].to_i,
+      d_key: key[3..4].to_i
+    }
+  end
+
 end
